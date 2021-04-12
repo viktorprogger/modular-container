e6e4fff41fbd45a9b1b41333baea1f002d5cab8d
viktorprogger <viktorprogger@gmail.com>
2021-04-12 19:16:48 +0300

    Finish with definition saving

--- src/RootContainer.php
+++ src/RootContainer.php
@@ -6,35 +6,51 @@ namespace Viktorprogger\Container;
 
 class RootContainer
 {
+    private const DEFINITIONS_KEY = '#definitions';
+
     /** @var array<string, ModuleContainer> */
     private array $containers = [];
-    private array $commonDefinitions;
+
+    private array $definitions = [];
 
     /**
      * @param array $definitions
      * Keys of a definition are:
      * - dependencies
      * - definitions
+     *     - module
+     *     - common
      * - submodules
      */
     public function __construct(array $definitions, array $commonDefinitions = [])
     {
-        $this->commonDefinitions = $commonDefinitions;
-
         foreach ($definitions as $namespace => $moduleConfig) {
             $this->build($namespace, $moduleConfig, $commonDefinitions);
         }
     }
 
-    private function build(string $namespace, array $definitions, array $commonDefinitions): void
+    private function build(string $namespace, array $moduleConfig, array $commonDefinitions): void
     {
-        $definitions = array_merge($commonDefinitions, $definitions);
-        $this->containers[$namespace] = new ModuleContainer($namespace, $definitions);
+        $commonDefinitions = array_merge($commonDefinitions, $moduleConfig['definitions']['common'] ?? []);
+        $definitions = array_merge($commonDefinitions, $moduleConfig['definitions']['module']);
+        $this->setDefinitions($namespace, $definitions);
 
-        $submodules = $moduleConfig['submodules'] ?? [];
-        foreach ($submodules as $subNamespace => $subDefinitions) {
-            // TODO Remove module-specific definitions from $definitions by namespace
-            $this->build("$namespace\\$subNamespace", $submodules, $definitions);
+        foreach ($moduleConfig['submodules'] ?? [] as $subNamespace => $subDefinitions) {
+            $this->build("$namespace\\$subNamespace", $subDefinitions, $commonDefinitions);
         }
+    }
+
+    private function setDefinitions(string $namespace, array $definitions): void
+    {
+        $definitionBag = &$this->definitions;
+        foreach (explode('\\', trim($namespace, '\\')) as $part) {
+            if (!isset($definitionBag[$part])) {
+                $definitionBag[$part] = [];
+            }
+
+            $definitionBag = &$definitionBag[$part];
+        }
+
+        $definitionBag[self::DEFINITIONS_KEY] = $definitions;
     }
 }
