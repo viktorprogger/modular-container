bee66cad88dca169445d6d85a2016f6361d0e0b8
viktorprogger <viktorprogger@gmail.com>
2021-04-13 13:53:16 +0300

    ModuleContainer

--- src/ModuleContainer.php
+++ src/ModuleContainer.php
@@ -2,18 +2,30 @@
 
 declare(strict_types=1);
 
-
 namespace Viktorprogger\Container;
 
+use InvalidArgumentException;
 use Psr\Container\ContainerInterface;
+use Yiisoft\Factory\Definitions\ArrayDefinition;
+use Yiisoft\Factory\Definitions\Normalizer;
 
 class ModuleContainer implements ContainerInterface
 {
+    private array $building = [];
+    private array $resolved = [];
+    private string $namespace;
+    private array $definitions;
+    private array $submoduleContainers = [];
+    private array $submoduleDefinitions;
+
     public function __construct(
         string $namespace,
         array $definitions,
-        ?ContainerInterface $commonContainer = null
+        array $submoduleDefinitions = []
     ) {
+        $this->namespace = $namespace;
+        $this->definitions = $definitions;
+        $this->submoduleDefinitions = $submoduleDefinitions;
     }
 
     /**
@@ -21,38 +33,116 @@ class ModuleContainer implements ContainerInterface
      */
     public function get($id)
     {
-        if ($this->moduleContainer->has($id)) {
-            return $this->moduleContainer->get($id);
+        return $this->resolved[$id] ?? $this->resolve($id);
+    }
+
+    public function has($id): bool
+    {
+        return $id === ContainerInterface::class
+            || isset($this->resolved[$id])
+            || isset($this->definitions[$id])
+            || strpos($id, $this->namespace) === 0;
+    }
+
+    /**
+     * @param string $id
+     *
+     * @return mixed
+     */
+    private function resolve(string $id)
+    {
+        if ($id === ContainerInterface::class) {
+            return $this;
+        }
+        $this->building[$id] = true;
+
+        if (isset($this->building[$id])) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Circular reference to "%s" detected while building: %s.',
+                    $id,
+                    implode(',', array_keys($this->building))
+                )
+            );
         }
 
-        foreach ($this->commonContainers->getContainers() as $container) {
-            if ($container->has($id)) {
-                return $container->get($id);
+        try {
+            if (isset($this->definitions[$id])) {
+                $this->resolved[$id] = $this->build($this->definitions[$id]);
+            } elseif (class_exists($id) && strpos($id, $this->namespace) === 0) {
+                $container = $this->getSubmoduleContainer($id);
+                if ($container === null) {
+                    $this->resolved[$id] = $this->build($id);
+                } else {
+                    $this->resolved[$id] = $container->get($id);
+                }
             }
-        }
-
-        foreach ($this->submoduleContainers->getContainers() as $container) {
-            if ($container->has($id)) {
-                return $container->get($id);
-            }
-        }
-
-        foreach ($this->thirdPartyContainers->getContainers() as $container) {
-            if ($container->has($id)) {
-                return $container->get($id);
-            }
+        } finally {
+            unset($this->building[$id]);
         }
 
         throw new NofFoundException();
     }
 
-    public function has(string $id): bool
+    private function build($definition)
     {
-        // TODO: Implement has() method.
+        if (is_string($definition)) {
+            $definition = new ArrayDefinition($definition);
+        } else {
+            $definition = Normalizer::normalize($definition);
+        }
+
+        return $definition->resolve($this);
     }
 
-    public function serve(string $id): bool
+    private function getSubmoduleContainer(string $id): ?ModuleContainer
     {
-        // TODO проверить, что либо $this->namespace входит в $id, либо $this->has($id), либо он есть у детей
+        $namespaceChosen = null;
+        $length = 0;
+
+        foreach ($this->submoduleDefinitions as $namespace => $definitions) {
+            $match = $this->getNamespaceMatch($id, $namespace);
+            if ($match > $length) {
+                $length = $match;
+                $namespaceChosen = $namespace;
+            }
+        }
+
+        if ($namespaceChosen !== null) {
+            if (!isset($this->submoduleContainers[$namespaceChosen])) {
+                $definitions = $this->submoduleDefinitions[$namespaceChosen];
+                $submodules = $definitions['#submodules'];
+                unset($definitions['#submodules']);
+
+                $this->submoduleContainers[$namespaceChosen] = new ModuleContainer(
+                    $namespaceChosen,
+                    $definitions,
+                    $submodules
+                );
+            }
+
+            return $this->submoduleContainers[$namespaceChosen];
+        }
+
+        return null;
+    }
+
+    private function getNamespaceMatch(string $className, string $namespace): int
+    {
+        $idNamespace = explode('\\', trim($className, '\\'));
+        array_pop($idNamespace); // remove class name
+
+        $namespaceDivided = explode('\\', $namespace);
+
+        $result = 0;
+        foreach ($namespaceDivided as $i => $part) {
+            if ($idNamespace[$i] === $part) {
+                $result++;
+            } else {
+                return $result;
+            }
+        }
+
+        return $result;
     }
 }
